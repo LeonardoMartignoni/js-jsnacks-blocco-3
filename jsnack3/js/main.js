6eaada08b5b54987a9e1e96e3ae24dc43501df5d
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-01-30 16:01:17 +0100

    Manipulated JSnack3 DOM

--- jsnack3/js/main.js
+++ jsnack3/js/main.js
@@ -2,29 +2,40 @@
 // Genera N array, ognuno formato da 10 numeri casuali da 1 a 100.
 // Ogni volta che ne crei uno, stampalo.
 
-// Chiedo all'utente un numero
-const userN = parseInt(prompt('Inserisci il numero degli array'))
+// Input Var
+const genButton = document.getElementById('gen_button');
 
+// Output Var
+const genResult = document.getElementById('generated_array');
 
-// Creo un array vuoto
-let userArray = [];
+genButton.addEventListener('click', function () {
 
-// Inizializzo l'indice
-let i = 0;
+    genResult.innerHTML = (``);
 
-// Creo N array
-while (i < userN) {
+    // Chiedo all'utente un numero
+    const userN = document.getElementById('array_num').value;
 
-    for (x = 0; x < 10; x++) {
-        const randomNumber = Math.floor(Math.random() * 100) + 1;
-        userArray.push(randomNumber);
+    // Creo un array vuoto
+    let userArray = [];
+
+    // Inizializzo l'indice
+    let i = 0;
+
+    // Creo N array
+    while (i < userN) {
+
+        for (x = 0; x < 10; x++) {
+            const randomNumber = Math.floor(Math.random() * 100) + 1;
+            userArray.push(randomNumber);
+        }
+
+        // Stampo l'array
+        genResult.innerHTML += (`Array: ${userArray} <br>`);
+
+        // Resetto l'array
+        userArray = [];
+
+        i++;
     }
+})
 
-    // Stampo l'array
-    console.log(userArray);
-
-    // Resetto l'array
-    userArray = [];
-
-    i++;
-}
